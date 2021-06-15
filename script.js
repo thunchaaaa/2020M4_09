54b5de8ed216372953038093cb1a18ee28efe7d7
thunchaaaaa <thunchanok_du@cmu.ac.th>
2021-06-15 16:58:00 +0700

    commit(1)
    commit(1)

--- script.js
+++ script.js
@@ -1,126 +1,39 @@
-// var searchButton = document.getElementById('searchButton')
-// var inputText = document.getElementById('inputText')
-// var output = document.getElementById('output')
-// var badgeCount = document.getElementById('count')
-// function addText(){
-//     let text = inputText.value
-//     console.log(text)
-//     let newButton = document.createElement('button')
-//     newButton.classList.add('btn')
-//     newButton.classList.add('btn-outline-primary')
-//     newButton.classList.add('m-2')
-//     newButton.setAttribute('type','button')
-//     newButton.innerText = text
-//     output.appendChild(newButton)
-//     badgeCount.innerText = output.children.length
-// }
+function addStudentData(index,student){
+    const output = document.getElementById('output')
 
-// searchButton.addEventListener('click',addText)
-// inputText.addEventListener('blur',addText)
-
-// var inputName = document.getElementById('name')
-// var inputUsername = document.getElementById('username')
-// var inputNamePrefix = document.getElementById('namePrefix')
-// var outputTableBody = document.getElementById('outputTableBody')
-// var submitData = document.getElementById('submitData')
-
-// function addDataToTable(index){
-//     let row = document.createElement('tr')
-//     let cell = document.createElement('th')
-//     cell.setAttribute('score',row)
-//     cell.innerHTML = index
-//     row.appendChild(cell)
-//     cell = document.createElement('td')
-//     cell.innerHTML = inputName.value
-//     row.appendChild(cell)
-//     cell = document.createElement('td')
-//     cell.innerHTML = inputUsername.value
-//     row.appendChild(cell)
-//     cell = document.createElement('td')
-//     cell.innerHTML = inputNamePrefix.options[inputNamePrefix.selectedIndex].text
-//     row.appendChild(cell)
-//     outputTableBody.appendChild(row)
-// }
-// var index = 1
-// submitData.addEventListener('click',(event)=>{
-        
-//     addDataToTable(index++)
-    
-// })
-var student = {};
-student.name='คุณลุง'
-student.username='a@b.com'
-student.gender='ชาย'
-document.getElementById('output').innerText=student;
-function addStudentData(student){
-    const output=document.getElementById('output')
-    let row = document.createElement('div')
-    row.classList.add("row")
-    let columnName=document.createElement('div')
-    columnName.classList.add("col-1")
-    columnName.classList.add("offset-1")
-    columnName.innerHTML='ชื่อ'
-    let columnValue = document.createElement('div')
-    columnValue=document.createElement('row')
-    columnValue.classList.add('col')
-    columnValue.innerHTML=student.name;
-    row.appendChild(columnName)
-    row.appendChild(columnValue)
-    output.appendChild(row)
-    
-    const output=document.getElementById('output')
-     row = document.createElement('div')
-    row.classList.add("row")
-    columnName=document.createElement('div')
-    columnName.classList.add("col-1")
-    columnName.classList.add("offset-1")
-    columnName.innerHTML='ชื่อ'
-    columnValue = document.createElement('div')
-    columnValue=document.createElement('row')
-    columnValue.classList.add('col')
-    columnValue.innerHTML=student.username;
-    row.appendChild(columnName)
-    row.appendChild(columnValue)
-    output.appendChild(row)
-
-    const output=document.getElementById('output')
-     row = document.createElement('div')
-    row.classList.add("row")
-     columnName=document.createElement('div')
-    columnName.classList.add("col-1")
-    columnName.classList.add("offset-1")
-    columnName.innerHTML='ชื่อ'
-     columnValue = document.createElement('div')
-    columnValue=document.createElement('row')
-    columnValue.classList.add('col')
-    columnValue.innerHTML=student.gender;
-    row.appendChild(columnName)
-    row.appendChild(columnValue)
-    output.appendChild(row)
-    
-}
-window.addEventListener("load",function(){
-    addStudentData(student)
-})
-function addStudentToTable(index,student){
-    const tableBody = document.getElementById('tableBody')
-    let row = document.createElement('tr')
-    let cell = document.createElement('th')
-    cell.setAttribute('socre','row')
-    cell.innerHTML=index
-    row.appendChild(cell)
-    cell=document.getElementById('td')
-    cell.innerHTML=student.name
-    row.appendChild(cell)
-    cell=document.getElementById('td')
-    cell.innerHTML=student.username
-    row.appendChild(cell)
-    cell=document.getElementById('td')
-    cell.innerHTML=student.gender
-    row.appendChild(cell)
-    tableBody.appendChild(row)
+   let row = document.createElement('tr')
+   let cell = document.createElement('th')
+   cell.setAttribute('score','row')
+   cell.innerHTML = index
+   row.appendChild(cell)
+   cell = document.createElement('td')
+   cell.innerHTML = student.name
+   row.appendChild(cell)
+   cell = document.createElement('td')
+   let image = document.createElement('img')
+   image.src = student.imageLink
+   image.width = 200
+   image.height = 200
+   cell.appendChild(image)
+   row.appendChild(cell)
+   cell = document.createElement('td')
+   cell.innerHTML = student.gender
+   row.appendChild(cell)
+   output.appendChild(row)
 
 }
-window.addEventListener("laod",function(){
-    addStudentToTable(student)
-})
+function addStudentList(StudentList){
+    let counter = 1;
+    for (student of StudentList){
+        addStudentData(counter++,student)
+    }
+}
+var x
+function onLoad(){
+    fetch('assets/students2.json').then(response =>{
+        return response.json()
+    }).then(data => {
+        addStudentList(data)
+    })
+    
+}
